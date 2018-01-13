79932115515cf69439ece7f55e1a1cab7b5c9d51
Eric Erhardt <eric.erhardt@microsoft.com>
2018-01-12 18:00:10 -0600

    Add project files.

---
+++ NamedPipeServerStreamBug.Server/Server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NamedPipeServerStreamBug.Server
+{
+    class Server
+    {
+        static ManualResetEvent waitHandle1 = new ManualResetEvent(false);
+        static ManualResetEvent waitHandle2 = new ManualResetEvent(false);
+        static void Main()
+        {
+            MainAsync().Wait();
+
+            Console.WriteLine("Done. Press Enter.");
+            Console.ReadLine();
+        }
+        static async Task MainAsync()
+        {
+            var backgroundTask = Task.Run((Action)BackgroundThread);
+            NamedPipeServerStream namedPipeServerStream = new NamedPipeServerStream("BugTest");
+            await namedPipeServerStream.WaitForConnectionAsync();
+
+            byte[] buffer = new byte[1024];
+            await namedPipeServerStream.ReadAsync(buffer, 0, buffer.Length);
+            namedPipeServerStream.RunAsClient(ImpersonationWorker);
+            await backgroundTask;
+        }
+
+        static void ImpersonationWorker()
+        {
+            waitHandle1.Set();
+            Console.WriteLine($"This should be run as the client. Running as '{Environment.UserName}'.");
+            waitHandle2.WaitOne();
+
+            Console.WriteLine("ImpersonationWorker is done.");
+        }
+
+        static void BackgroundThread()
+        {
+            Console.WriteLine($"BackgroundThread started. Running as: '{Environment.UserName}'.");
+            waitHandle1.WaitOne();
+            Console.WriteLine($"BackgroundThread continued. Running as: '{Environment.UserName}'.");
+            waitHandle2.Set();
+        }
+    }
+}
---
+++ NamedPipeServerStreamBug/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedPipeServerStreamBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            NamedPipeClientStream pipe = new NamedPipeClientStream("BugTest");
+            await pipe.ConnectAsync();
+            await pipe.WriteAsync(Encoding.UTF8.GetBytes("hello"), 0, Encoding.UTF8.GetByteCount("hello"));
+        }
+    }
+}
